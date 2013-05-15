916f3edb61569aab8b597a634d5375eeee7330bb
Topher Brown <topher200@gmail.com>
2013-05-15 18:49:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RipulDraft::Application.config.secret_token = '755f06b195b9ebb6f368aa0427e2725c90bd47d4ad418c388970e91227edb3b8d8b5632ab8f1d93a4ccb5d450ea25b44e366763863ada99ef63a8c0d3418df58'
