760a3c8705720d2bddfd542eaf2bb92f811c34bc
Topher Brown <topher200@gmail.com>
2013-05-20 05:51:30 -0400

    seeds: add teams.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,21 @@
 require 'csv'
 
 players_filename = "#{Rails.root}/lib/assets/ripul.csv"
+num_players = 0
 CSV.foreach(players_filename, :headers => :first_row) do |player|
-  puts "processing #{player['first']} #{player['last']}"
+  # puts "processing #{player['first']} #{player['last']}"
   p = Player.create(:first_name => player['first'], :last_name => player['last'],
                     :age => player['age'], :gender => player['gender'])
   p.save
+  num_players += 1
 end
+puts "processed #{num_players} players"
+
+teams_filename = "#{Rails.root}/lib/assets/teams.csv"
+num_teams = 0
+CSV.foreach(teams_filename, :headers => :first_row) do |team|
+  # puts "processing #{team['color']}"
+  team = Team.create(:color => team['color'])
+  num_teams += 1
+end
+puts "processed #{num_teams} teams"
