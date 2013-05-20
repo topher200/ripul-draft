c6cf73fede167233f1c3a1c57915e9ae5a7acede
Topher Brown <topher200@gmail.com>
2013-05-20 17:56:22 -0400

    player: add name()

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,7 @@
 class Player < ActiveRecord::Base
   belongs_to :team
   attr_accessible :age, :first_name, :gender, :last_name
+  def name
+    return self.first_name + " " + self.last_name
+  end
 end
