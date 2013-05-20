e0fc8d0b4323976ab8d3fd9ba578e1018613ea3d
Topher Brown <topher200@gmail.com>
2013-05-20 05:17:48 -0400

    seeds: load players from ripul.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+players_filename = "#{Rails.root}/lib/assets/ripul.csv"
+CSV.foreach(players_filename, :headers => :first_row) do |player|
+  puts "processing #{player['first']} #{player['last']}"
+  p = Player.create(:first_name => player['first'], :last_name => player['last'],
+                    :age => player['age'], :gender => player['gender'])
+  p.save
+end
