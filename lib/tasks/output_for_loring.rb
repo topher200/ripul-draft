9aec328f941be2eb1799156010c1fc7ed841e2b9
Topher Brown <topher200@gmail.com>
2013-05-22 08:17:50 -0400

    output_for_loring: output ripul id

--- lib/tasks/output_for_loring.rb
+++ lib/tasks/output_for_loring.rb
@@ -6,13 +6,13 @@ require 'csv'
 puts "starting"
 num_saved = 0
 CSV.open("draft_list.csv", 'w') do |csv|
-  csv << ["first_name", "last_name", "team"]
+  csv << ["first_name", "last_name", "ripul_db_id_number", "team"]
   Player.all.each do |player|
     if not player.first_name or not player.last_name or not player.team or not player.team.color
       puts "error processing #{player.name}"
       next
     end
-    csv << [player.first_name, player.last_name, player.team.color]
+    csv << [player.first_name, player.last_name, player.ripul_id_number, player.team.color]
     num_saved += 1
   end
 end
