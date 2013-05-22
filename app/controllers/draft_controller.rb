b9fa599dbf819fd06644a76c39d7b662a4f5f377
Topher Brown <topher200@gmail.com>
2013-05-22 03:52:15 -0400

    route all draft picks through index

--- app/controllers/draft_controller.rb
+++ app/controllers/draft_controller.rb
@@ -1,4 +1,13 @@
 class DraftController < ApplicationController
   def index
+    if params[:player]
+      redirect_to draft_path
+    end
+
+    @next_pick = Pick.next_pick
+    @information_string = "Drafting #{@next_pick.gender_to_string} " \
+                          "##{@next_pick.number} " \
+                          "to team #{@next_pick.team.color}"
+    @players = Player.undrafted
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 RipulDraft::Application.routes.draw do
   resources :baggages
-
-
   resources :picks
   resources :players
   resources :teams
-  match "draft" => "draft#index"
 
+  get "draft" => "draft#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
