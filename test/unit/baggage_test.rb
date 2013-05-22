0be42188b4331d69d26faa5adbdd3c72201a19c6
Topher Brown <topher200@gmail.com>
2013-05-22 05:13:06 -0400

    baggages model: add tests for new functions

--- test/unit/baggage_test.rb
+++ test/unit/baggage_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class BaggageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "other_player_in_baggage is correct" do
+    assert baggages(:one).other_player_in_baggage(baggages(:one).player_one) == baggages(:one).player_two
+    assert baggages(:one).other_player_in_baggage(baggages(:one).player_two) == baggages(:one).player_one
+  end
 end
