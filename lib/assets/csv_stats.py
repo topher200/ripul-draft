7dba0d3069bdf8d383ff3c682950d0198e924927
Topher Brown <topher200@gmail.com>
2014-05-21 05:10:13 -0400

    add csv_stats, to match picks and player numbers

---
+++ lib/assets/csv_stats.py
@@ -0,0 +1,36 @@
+import re
+
+print "processing players"
+num_males = 0
+num_females = 0
+with open('ripul.csv', 'r') as file:
+    for line in file:
+        if "last,first,gender" in line:
+            print "skipping first line"
+        elif ",M," in line:
+            num_males += 1
+        elif ",F," in line:
+            num_females += 1
+        else:
+            print "error processing line: {}".format(line)
+# We don't draft any captains, save for two of the males
+print "males: {}. drafting {}".format(num_males, num_males - 16 + 2)
+print "females: {}. drafting {}".format(num_females, num_females - 16)
+
+print "-----"
+
+print "processing picks"
+num_males = 0
+num_females = 0
+with open('picks.csv', 'r') as file:
+    for line in file:
+        if "gender,ign" in line:
+            print "skipping first line"
+        elif re.match("M,", line):
+            num_males += 1
+        elif re.match("F,", line):
+            num_females += 1
+        else:
+            print "error processing line: {}".format(line)
+print "males: {}".format(num_males)
+print "females: {}".format(num_females)
