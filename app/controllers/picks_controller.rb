b08aacaa2f6bd94d91b16101d11a8b27317ae5e1
Topher Brown <topher200@gmail.com>
2015-05-08 02:30:17 -0400

    undo Picks with :update instead of :destroy

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -57,30 +57,26 @@ class PicksController < ApplicationController
   # PUT /picks/1
   # PUT /picks/1.json
   def update
+    # We undo the pick
     @pick = Pick.find(params[:id])
+    logger.info "Undoing Pick #{@pick}"
+    player = Player.find(@pick.player)
+    player.team = nil
+    player.save
+    @pick.player = nil
+    @pick.save
 
     respond_to do |format|
-      if @pick.update_attributes(params[:pick])
-        format.html { redirect_to @pick, notice: 'Pick was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @pick.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to picks_url }
+      format.json { head :no_content }
     end
   end
 
   # DELETE /picks/1
   # DELETE /picks/1.json
   def destroy
-    # We don't destroy, we just undo the pick
     @pick = Pick.find(params[:id])
-    logger.info "Deleting Pick #{@pick}"
-    player = Player.find(@pick.player)
-    player.team = nil
-    player.save
-    @pick.player = nil
-    @pick.save
+    @pick.destroy
 
     respond_to do |format|
       format.html { redirect_to picks_url }
