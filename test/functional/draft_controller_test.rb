d52819d0f62ac68110cec97d6612b21e7ed4352e
Topher Brown <topher200@gmail.com>
2015-05-09 15:54:16 -0400

    draft_test: confirm that the next pick gets a player

--- test/functional/draft_controller_test.rb
+++ test/functional/draft_controller_test.rb
@@ -9,16 +9,20 @@ class DraftControllerTest < ActionController::TestCase
   test "should get index" do
     get :index, gender: @gender
     assert_response :success
-    assert_not_nil assigns(:gender)
-    assert_not_nil assigns(:players)
+    assert_not_nil assigns(:gender), "Gender isn't set"
+    assert_not_nil assigns(:players), "Players aren't set"
   end
 
-  test "should make pick" do
+  test "should make next pick" do
+    # The next pick (in order) should start with no player but end up with one.
+    our_pick = Pick.next_undrafted(@player.gender)
+    assert_nil our_pick.player, "Pick already has a Player"
+
     # We start with an undrafted player and end up with a drafted one
-    assert_nil Player.find(@player).team
-
+    assert_nil Player.find(@player).team, "Player already has a team"
     post :index, gender: @gender, player: @player
+    assert_not_nil Player.find(@player).team, "Player didn't get a team"
 
-    assert_not_nil Player.find(@player).team
+    assert_not_nil Pick.find(our_pick).player, "Pick didn't get a Player"
   end
 end
