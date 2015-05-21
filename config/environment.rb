d89eba4bce6ec75c7c671a7fe829b73ff5faefb2
Topher Brown <topher200@gmail.com>
2015-05-21 16:38:05 -0400

    use production environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RipulDraft::Application.initialize!
+
+# Set environment
+ENV['RAILS_ENV'] ||= 'production'
